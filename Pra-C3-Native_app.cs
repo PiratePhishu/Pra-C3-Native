06dc64cdcc96bff5a7cea91e88cc12aa726d51c9
D302620 <150166438+D302620@users.noreply.github.com>
2024-11-24 14:01:32 +0100

    loggedIn variabele weg gehaald en veranderd

--- Pra-C3-Native_app.cs
+++ Pra-C3-Native_app.cs
@@ -13,91 +13,146 @@ namespace Pra_C3_Native
     internal class Pra_C3_Native_app
     {
         NativeContext Datacontext;
-        User session = new User();
-        bool loggedIn = false;
+        User? session = null;
 
-        public Pra_C3_Native_app() 
+        public Pra_C3_Native_app()
         {
             Datacontext = new NativeContext();
         }
         public void Run()
         {
             string userInput = "";
-      
-            while (userInput.ToLower() != "x") 
+
+            while (userInput.ToLower() != "x")
             {
-                userInput = ShowMenu(loggedIn);
-                loggedIn = HandleInput(userInput, loggedIn);
+                userInput = ShowMenu();
+                HandleInput(userInput);
             }
         }
 
-        public bool HandleInput(string userInput, bool loggedIn)
+        public void HandleInput(string userInput)
         {
-            if (loggedIn)
+            if (session != null)
             {
-                switch(userInput) 
+                switch (userInput)
                 {
                     case "1":
                         break;
                     case "2":
                         break;
+                    case "3":
+                        session = null;
+                        break;
                     default:
                         Console.WriteLine("kies een geldige keuze");
                         break;
                 }
-                return true;
             }
             else
             {
                 switch (userInput)
                 {
                     case "1":
-                        loggedIn = RegisterAccount();
-                        return loggedIn;
+                        RegisterAccount();
+                        break;
+                    case "2":
+                        logIn();
+                        break;
                     default:
                         Console.WriteLine("kies een geldige keuze");
-                        return false;
+                        break;
                 }
             }
         }
 
-        public bool RegisterAccount()
+        public void RegisterAccount()
         {
-            Console.Clear();
-            User user = new User();
-            Console.WriteLine("enter Username");
-            user.Name = Console.ReadLine();
-            Console.WriteLine("enter email");
-            user.Email = Console.ReadLine().ToLower();
-            Console.WriteLine("enter password");
-            string password = Console.ReadLine();
-            Console.WriteLine("repeat password");
-            string passwordRepeat = Console.ReadLine();
-            if (password == passwordRepeat) {
-                user.PasswordHash(password);
-                Datacontext.Add(user);
-                Datacontext.SaveChanges();
-                Console.WriteLine("user has been added");
-                return true;
-                
-            }
-            else
+            bool found = false;
+            while(session == null) 
             {
-                Console.WriteLine("username was not the same");
-                return false;
+                found = false;
+                Console.Clear();
+                User user = new User();
+                Console.WriteLine("enter Username:");
+                user.Name = Console.ReadLine();
+                List<User> users = Datacontext.users.ToList();
+                foreach (User userName in users)
+                {
+                    if(userName.Name == user.Name)
+                    {
+                        Console.WriteLine("username is al in gebruik");
+                        Console.ReadLine();
+                        found = true;
+                    }
+                }
+                if (found)
+                {
+                    continue;
+                }
+                Console.WriteLine("\nenter email:");
+                user.Email = Console.ReadLine().ToLower();
+                Console.WriteLine("\nenter password:");
+                string password = Console.ReadLine();
+                Console.WriteLine("\nrepeat password:");
+                string passwordRepeat = Console.ReadLine();
+                if (password == passwordRepeat)
+                {
+                    user.PasswordHash(password);
+                    Datacontext.Add(user);
+                    Datacontext.SaveChanges();
+                    Console.WriteLine("user has been added");
+                    Console.ReadLine();
+                    session = user;
+                }
+                else
+                {
+                    Console.WriteLine("username was not the same");
+                    Console.ReadLine();
+                }
             }
+        }
+
+        public void logIn()
+        {
+            string userinput;
+            List<User> userList = Datacontext.users.ToList();
+
+            Console.Clear();
+            Console.WriteLine("please enter your username:");
+            userinput = Console.ReadLine();
+            foreach(User user in userList)
+            {
+                if (user.Name == userinput)
+                {
+                    Console.WriteLine("please enter your password");
+                    string password = Console.ReadLine();
+                    if (BCrypt.Net.BCrypt.EnhancedVerify(password, user.Password))
+                    {
+                        session = user;
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("onjuist wachtwoord");
+                        Console.ReadLine();
+                    }
+                }
+            }
+            Console.WriteLine("gebruiker niet gevonden");
+            Console.ReadLine();
 
         }
 
-        
-
-        public string ShowMenu(bool loggedIn)
+        public string ShowMenu()
         {
             Console.Clear();
-            if (loggedIn)
+            if (session != null)
             {
+                Console.WriteLine(session.Name);
+                Console.WriteLine(session.Credits);
                 Console.WriteLine("1. show all matches");
                 Console.WriteLine("2. show user account");
+                Console.WriteLine("3. log out");
             }
             else
             {
